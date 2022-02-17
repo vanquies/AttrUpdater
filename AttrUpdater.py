f8e35b1b9f4bd558bab904e6b8e01d3d0a31415a
Bartek <brtkrokita@gmail.com>
2022-02-17 11:49:28 +0100

    Update 17 Feb

--- AttrUpdater.py
+++ AttrUpdater.py
@@ -1,3 +1,7 @@
+# This program was created to replace attributes
+# in YAML file (processors/resource/attributes)
+# with Web Request attributes from XML file.
+
 import xml.etree.ElementTree as ET
 import yaml
 
@@ -10,41 +14,49 @@ try:
             self.file_yaml = file_yaml
 
         def value_replace(self):
-            requests = {}
+            request = {}
             for child in self.root.iter('WebRequest'):
                 for i in range(len(child)):
                     if child[i].text:
-                        requests[child[i].tag.lower()] = child[i].text
+                        request[child[i].tag.lower()] = child[i].text
 
-            old_type = 'requesttype'
-            new_type = 'request.type'
+            try:
+                if len(request) == 0:
 
-            old_code = 'statuscode'
-            new_code = 'app.status_code'
+                    raise Exception("The XML file has no web request attributes. Try another file.")
 
-            requests[new_type] = requests.pop(old_type)
-            requests[new_code] = requests.pop(old_code)
+                old_type = 'requesttype'
+                new_type = 'request.type'
+
+                old_code = 'statuscode'
+                new_code = 'app.status_code'
+
+                request[new_type] = request.pop(old_type)
+                request[new_code] = request.pop(old_code)
+
+            except Exception as error:
+                print(error)
 
             with open(self.file_yaml) as f:
                 data_map = yaml.safe_load(f)
 
-            for dicts in data_map['processors']['resource']['attributes']:
-                for tags, texts in requests.items():
-                    if tags == dicts.get("key"):
-                        if texts.isnumeric():
-                            dicts["value"] = int(texts)
-                        else:
-                            dicts["value"] = texts
+            try:
+                for dicts in data_map['processors']['resource']['attributes']:
+                    for tags, texts in request.items():
+                        if tags == dicts.get("key"):
+                            if texts.isnumeric():
+                                dicts["value"] = int(texts)
+                            else:
+                                dicts["value"] = texts
+
+            except TypeError:
+                print("The YAML file does not have the appropriate attributes.")
 
             with open(self.file_yaml, 'w') as f:
                 yaml.safe_dump(data_map, f, sort_keys=False, default_flow_style=False)
 
+            print("The file has been updated successfully!")
             return self.file_yaml
-
-
-    d1 = AttrUpdater('xml_test_file.xml', 'zadanko_bartek.yaml')
-    print(d1.value_replace())
 
 except FileNotFoundError:
     print("Indicated file(s) doesn't exist")
-
